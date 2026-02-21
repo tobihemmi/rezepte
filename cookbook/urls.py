1f4f344a74550483dd88d4c9252d13abc53659bc
Tobias Hemmeke <tobias.hemmeke@gmx.de>
2026-02-21 19:44:47 +0100

    next try

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path('recipes/', include("recipes.urls")),
@@ -26,6 +26,8 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
     path('', RedirectView.as_view(url='/recipes/', permanent=True)),
+    path('sw.js', TemplateView.as_view(template_name="sw.js", content_type='application/javascript'), name='sw.js'),
+    path('manifest.json', TemplateView.as_view(template_name="manifest.json", content_type='application/json'), name='manifest.json'),
 ]
 
 
