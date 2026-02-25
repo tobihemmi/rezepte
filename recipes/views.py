b552bf7e6bbd848e2268b29cb38c8289c3c414a5
Tobias Hemmeke <tobias.hemmeke@gmx.de>
2026-02-25 11:16:23 +0100

    fix cook mode issue and styling issues

--- recipes/views.py
+++ recipes/views.py
@@ -16,9 +16,6 @@ from django.contrib.auth.views import redirect_to_login
 from datetime import date, timedelta, datetime
 
 
-DAYS = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
-
-
 def filter_recipes(qs, params):
     # 🔎 Suche
     query = params.get("q")
@@ -260,14 +257,22 @@ class RecipeCookView(generic.DetailView):
                 current_servings = base_servings
         except (TypeError, ValueError):
             current_servings = base_servings
+        try:
+            ing_list = [line.strip() for line in recipe.ingredients.splitlines() if line.strip()]
+        except BaseException:
+            ing_list = []
+        try:
+            st_list = [line.strip() for line in recipe.steps.splitlines() if line.strip()]
+        except BaseException:
+            st_list = []
 
         # Zutaten unverändert übergeben → JS skaliert
         context.update({
             "recipe": recipe,
             "base_servings": base_servings,
             "current_servings": current_servings,
-            "ingredients_list": [line.strip() for line in recipe.ingredients.splitlines() if line.strip()],
-            "steps_list": [line.strip() for line in recipe.steps.splitlines() if line.strip()],
+            "ingredients_list": ing_list,
+            "steps_list": st_list,
         })
         return context
 
